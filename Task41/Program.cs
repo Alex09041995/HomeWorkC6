15449262ecd507627ae76eea0e7b6a7f3137655f
Alexander <shmygarevalexander009@gmail.com>
2023-01-28 06:06:49 +0500

    HomeWork6

---
+++ Task41/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Данная программа принимает с клавиатуры определённое количество чисел М. И считает сколько чисел больше 0 ввёл пользователь.");
+Console.Write("Введите любое количество чисел через запятую: ");
+
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+
+int[] StringToNum(string input)
+{
+int count = 1;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == ',')
+    {
+        count++;
+    }
+}
+
+int[] numbers = new int [count];
+int index = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    string temp = "";
+
+while (input [i] != ',')
+{
+if(i != input.Length - 1)
+{
+    temp += input [i].ToString();
+    i++;
+    }
+    else
+    {
+        temp += input [i].ToString();
+        break;
+    }
+}
+    numbers[index] = Convert.ToInt32(temp);
+    index++;
+}
+return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Введены следующие числа: [");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write("]");
+}
+Console.WriteLine();
+Console.WriteLine($"Количество введённых чисел, значение которых больше 0: {sum}");
---
+++ Task43/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Данная программа находит точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2. Значения для уравнения: b1, k1, b2 и k2 задаются пользователем.");
+
+Console.Write("Введите любое значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите любое значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите любое значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите любое значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = (k1 * x + b1);
+
+Console.WriteLine($"Точкой пересечения двух прямых - является точка с координатами X: {x}, Y: {y}");
+
+if ((k1 == k2) && (b1 == b2))
+{
+Console.WriteLine($"Координаты прямых совпадают!");
+}
+else if (k1 == k2)
+{
+Console.WriteLine($"Координаты прямых параллельны!");
+}
